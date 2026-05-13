198dfd1256fee2e01d5285a5a2cb3bddc530fd23
Oieusouopi <rafasc866@gmail.com>
2026-05-13 20:13:52 -0300

    novo executar ciclo

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
 	printf("Valor da memoria antes %u\n", cpu.memoria.memorias[0]);
 	printf("Valor do registrador antes %u\n", cpu.registradores[0].valor_atual);
 	armazenar(&cpu.memoria, 0, 100);
-	executar_ciclo(&cpu, 0);
+	executar_ciclo(&cpu);
 	printf("Valor da memoria depois %u\n", cpu.memoria.memorias[0]);
 	printf("Valor do registrador depois %u\n", cpu.registradores[0].valor_atual);
 	return 0;
