bca1fc1da4de695a99f7c225b92813d7d5a155ec
Oieusouopi <rafasc866@gmail.com>
2026-05-13 00:23:40 -0300

    header: metodo de incializacao e estrutura da CPU

--- cpu.h
+++ cpu.h
@@ -1,5 +1,17 @@
 #ifndef  CPU_H
 #define CPU_H
+#include "memory.h"
+#include "register.h"
+#include "config.h"
+
+typedef struct {
+    RegistradorEstrutura registradores[QUANTIDADE_DE_REGISTRADORES];
+    MemoriaEstrutura memoria;
+} CPUEstrutura;
+
+CPUEstrutura inicializacao();
+
+void executar_ciclo(CPUEstrutura *cpu, uint8_t endereco_de_memoria);
 
 
 #endif
