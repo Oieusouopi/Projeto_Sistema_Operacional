e0b44b7edf93fc2612e442b567f6c57772cd7a30
Oieusouopi <rafasc866@gmail.com>
2026-05-13 00:57:19 -0300

    teste de incializacao

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+#include "cpu.h"
+
 int main() {
-	
-
-	nand 1100, 1000;
+	CPUEstrutura cpu = inicializacao();
+	printf("Valor da memoria antes %u\n", cpu.memoria.memorias[0]);
+	printf("Valor do registrador antes %u\n", cpu.registradores[0].valor_atual);
+	armazenar(&cpu.memoria, 0, 100);
+	executar_ciclo(&cpu, 0);
+	printf("Valor da memoria depois %u\n", cpu.memoria.memorias[0]);
+	printf("Valor do registrador depois %u\n", cpu.registradores[0].valor_atual);
 	return 0;
-
 };
