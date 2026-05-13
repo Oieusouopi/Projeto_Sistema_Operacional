625b4eeaf1b4ffff2be7720682ead1cc31577231
Oieusouopi <rafasc866@gmail.com>
2026-05-13 00:24:00 -0300

    metodo de iniciazalicao da cpu

--- cpu.c
+++ cpu.c
@@ -1,6 +1,27 @@
-int main() {
+#include "cpu.h"
 
-    
+#include <stdio.h>
+#include <string.h>
 
-    return 0;
+CPUEstrutura inicializacao() {
+    CPUEstrutura cpu;
+    MemoriaEstrutura memoria_estrutura;
+
+    for (int i = 0; i < TAMANHO_MEMORIA; i++) {
+        memoria_estrutura.memorias[i] = 0;
+    }
+
+    for (int i = 0; i < QUANTIDADE_DE_REGISTRADORES; i++) {
+        RegistradorEstrutura registrador;
+        char nome[3];
+        sprintf(nome, "R%d", i);
+        strcpy(registrador.nome, nome);
+        escrever(&registrador, 0);
+
+        cpu.registradores[i] = registrador;
+    }
+
+    cpu.memoria = memoria_estrutura;
+
+    return cpu;
 }
