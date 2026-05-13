26a49fe45135a1558d26b1f21a76617b8812524b
Oieusouopi <rafasc866@gmail.com>
2026-05-13 20:13:19 -0300

    status da cpu

--- cpu.h
+++ cpu.h
@@ -4,14 +4,21 @@
 #include "register.h"
 #include "config.h"
 
+typedef enum {
+    PARADO,
+    RODANDO
+} StatusCPU;
+
 typedef struct {
     RegistradorEstrutura registradores[QUANTIDADE_DE_REGISTRADORES];
     MemoriaEstrutura memoria;
+    RegistradorEstrutura pc;
+    StatusCPU rodando;
 } CPUEstrutura;
 
 CPUEstrutura inicializacao();
 
-void executar_ciclo(CPUEstrutura *cpu, uint8_t endereco_de_memoria);
+void executar_ciclo(CPUEstrutura *cpu);
 
 
 #endif
