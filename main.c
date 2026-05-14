60cc51bd5b34aa7320caadc4819b263696d5ce6b
Oieusouopi <rafasc866@gmail.com>
2026-05-13 23:47:19 -0300

    inicializacao e execucao do ciclo

--- main.c
+++ main.c
@@ -4,11 +4,24 @@
 
 int main() {
 	CPUEstrutura cpu = inicializacao();
-	printf("Valor da memoria antes %u\n", cpu.memoria.memorias[0]);
-	printf("Valor do registrador antes %u\n", cpu.registradores[0].valor_atual);
-	armazenar(&cpu.memoria, 0, 100);
+	cpu.memoria.memorias[0] = 0x01;
+	cpu.memoria.memorias[1] = 0x00;
+	cpu.memoria.memorias[2] = 20;
+
+	cpu.memoria.memorias[3] = 0x01;
+	cpu.memoria.memorias[4] = 0x01;
+	cpu.memoria.memorias[5] = 21;
+
+	cpu.memoria.memorias[6] = 0x02;
+	cpu.memoria.memorias[7] = 0x00;
+	cpu.memoria.memorias[8] = 0x01;
+
+	cpu.memoria.memorias[9] = 0x04;
+
+	cpu.memoria.memorias[20] = 20;
+	cpu.memoria.memorias[21] = 10;
+
 	executar_ciclo(&cpu);
-	printf("Valor da memoria depois %u\n", cpu.memoria.memorias[0]);
-	printf("Valor do registrador depois %u\n", cpu.registradores[0].valor_atual);
+	printf("Resultado R0: %u\n", cpu.registradores[0].valor_atual);
 	return 0;
 };
